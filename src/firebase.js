63de01114734cab1adcee4295c388feab3c8b152
Ifechukwu Edet <ifechiglory@gmail.com>
2025-06-28 03:20:37 +0100

    bug fixes in admin page

--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL
 };
 
 const app = initializeApp(firebaseConfig);
